e1c3d4d6acb38ca487645dc5bb1d580ef9ca9892
Stanislav Rybar <Zdenky338@NorthAmericanLaptop.local>
2023-01-25 09:11:47 -0500

    complete projects section & initial ProjectCard component

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Outerspace personal portfolio
 ![](/public/outerspace-personal-portfolio.png)
 Tutorial URL: https://www.youtube.com/watch?v=hYv6BM2fWd8&t=804s
+
 Tutorial description: In this tutorial, we build a personal portfolio website using React and Animate CSS.
 
 ___________
@@ -10,7 +11,7 @@ Bootstrap: 5.2.3
 React: 18.2.0
 
 ## Problems in the project:
-1.
+1. Hard to view whether it was an h5 or h2 in the CSS.
 2.
 
 ## How to use
@@ -20,9 +21,12 @@ React: 18.2.0
 
 
 ## Photos of project:
+![](/public/skills-section.png)
 
 ## Apps used:
+- Visual Studio Code 2
 
 ## Coding styles I used:
+- Functional components in all components.
 
 ## What I learned
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,83 @@
+import { Nav } from "react-bootstrap";
+
 export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development"  ,
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3  
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: "projImg2"
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3
+        }
+    ];
     return (
-        
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" defaultActiveKey="/home">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third" disabled>
+                                        Tab Three
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <p>{project.title}</p>
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Lorem ipsum</Tab.Pane>
+                                <Tab.Pane eventKey="third">Lorem ipsum</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
